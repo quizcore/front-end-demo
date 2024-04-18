4dcfaeb0a4720fcdf671f13309b14a3d3d8bd9c4
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-04-17 23:01:46 -0700

    edit exam page

--- exam.php
+++ exam.php
@@ -1,65 +1,65 @@
 <?php
-    $pageTitle = "Exam";
-    require_once 'header.php';
+$pageTitle = "Exam";
+require_once 'testheader.php';
 ?>
 
 <!--Main-->
-<div id="myForm" class="form-signin w-100 m-auto">
-  <form>
-    <h1 class="h3 mb-3 fw-normal">Filling your information</h1>
+<!--Main Div-->
+<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+  <!--Form-->
+  <div id="myForm" class="form-signin w-100 m-auto">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Filling your information</h1>
 
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="First Name">
-      <label for="floatingInput">First Name</label>
+      <div class="form-floating">
+        <input type="text" class="form-control" id="floatingInput" placeholder="First Name">
+        <label for="floatingInput">First Name</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="text" class="form-control" id="floatingInput" placeholder="Last Name">
+        <label for="floatingInput">Last Name</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="date" class="form-control" id="floatingPassword" placeholder="date of birth">
+        <label for="floatingPassword">Date of Birth</label>
+      </div><br />
+
+      <button class="btn btn-lg btn-dark w-100 py-2" type="submit">Sign Up</button>
+    </form>
+  </div>
+
+
+  <!--Quick Start Button>-->
+  <div id="quickStartBtn>" class="form-signin w-100 m-auto">
+    <!--add some text-->
+    <div>
+      <h1></h1>
     </div>
+    <a href="exam2.php" class="btn btn-lg btn-bd-red w-100 py-2">Quick Start</a>
+  </div>
 
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Last Name">
-      <label for="floatingInput">Last Name</label>
-    </div>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-
-    <div class="form-floating">
-      <input type="date" class="form-control" id="floatingPassword" placeholder="date of birth">
-      <label for="floatingPassword">Date of Birth</label>
-    </div><br />
-
-    <button class="btn btn-lg btn-bd-red w-100 py-2" type="submit">Take Exam</button>
-  </form>
 </div>
 
+
+
 <!--Dark mode-->
-<div
-  class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle"
->
-  <button
-    class="btn btn-secondary py-2 dropdown-toggle d-flex align-items-center"
-    id="bd-theme"
-    type="button"
-    aria-expanded="false"
-    data-bs-toggle="dropdown"
-    aria-label="Toggle theme (auto)"
-  >
+<div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+  <button class="btn btn-secondary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
     <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
       <use href="#circle-half"></use>
     </svg>
     <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
   </button>
-  <ul
-    class="dropdown-menu dropdown-menu-end shadow"
-    aria-labelledby="bd-theme-text"
-  >
+  <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
     <li>
-      <button
-        type="button"
-        class="dropdown-item d-flex align-items-center"
-        data-bs-theme-value="light"
-        aria-pressed="false"
-      >
+      <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
         <svg class="bi me-2 opacity-50" width="1em" height="1em">
           <use href="#sun-fill"></use>
         </svg>
@@ -70,12 +70,7 @@
       </button>
     </li>
     <li>
-      <button
-        type="button"
-        class="dropdown-item d-flex align-items-center"
-        data-bs-theme-value="dark"
-        aria-pressed="false"
-      >
+      <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
         <svg class="bi me-2 opacity-50" width="1em" height="1em">
           <use href="#moon-stars-fill"></use>
         </svg>
@@ -86,12 +81,7 @@
       </button>
     </li>
     <li>
-      <button
-        type="button"
-        class="dropdown-item d-flex align-items-center active"
-        data-bs-theme-value="auto"
-        aria-pressed="true"
-      >
+      <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
         <svg class="bi me-2 opacity-50" width="1em" height="1em">
           <use href="#circle-half"></use>
         </svg>
@@ -107,7 +97,16 @@
 <script>
   document
     .getElementById("myForm")
-    .addEventListener("submit", function (event) {
+    .addEventListener("submit", function(event) {
+      // Prevents the default form submission behavior.
+      event.preventDefault();
+      // Redirect to exam2.php.
+      window.location.href = "exam2.php";
+    });
+
+  document
+    .getElementById("quickStartBtn")
+    .addEventListener("submit", function(event) {
       // Prevents the default form submission behavior.
       event.preventDefault();
       // Redirect to exam2.php.
@@ -117,6 +116,6 @@
 <!--End of Main-->
 
 <?php
-    // Include footer.
-    require_once 'footer.php';
+// Include footer.
+require_once 'testfooter.php';
 ?>
---
+++ testfooter.php
@@ -0,0 +1,16 @@
+    <!--footer-->
+    <div class="container-fluid py-5">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <a href="#" class="col-md-6 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <img src="img/cwu-logo-full-color-rgb-transparent.png" height="40px" />
+        </a>
+        <p class="col-md-6 mb-0 text-body-secondary">
+          &copy; 2024 Central Washington University
+        </p>
+
+      </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
